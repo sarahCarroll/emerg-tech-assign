6a931b161a933c0c23071b31c1f6e88c790b7704
sarahc123 <g00330821@gmit.ie>
2018-11-26 15:14:36 +0000

    mnist neural network(nn) getting back 0.95+ accuracy

--- digitrec.py
+++ digitrec.py
@@ -7,14 +7,15 @@ import gzip
 model = kr.models.Sequential()
 
 # Add a hidden layer with 1000 neurons and an input layer with 784.
-model.add(kr.layers.Dense(units=600, activation='sigmoid', input_dim=784))
-model.add(kr.layers.Dense(units=400, activation='relu', input_dim=784))
+model.add(kr.layers.Dense(units=600, activation='linear', input_dim=784))
+model.add(kr.layers.Dense(units=500, activation='sigmoid', input_dim=784))
+model.add(kr.layers.Dense(units=300, activation='relu', input_dim=784))
 # Add a three neuron output layer.
 model.add(kr.layers.Dense(units=10, activation='softmax'))
 
 # Build the graph.
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
 
 with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
     train_img = f.read()
@@ -33,4 +34,16 @@ encoder = pre.LabelBinarizer()
 encoder.fit(train_lbl)
 outputs = encoder.transform(train_lbl)
 
-model.fit(inputs, outputs, epochs=4, batch_size=100)
+model.fit(inputs, outputs, epochs=7, batch_size=98)
+
+with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+    test_img = f.read()
+
+with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+    test_lbl = f.read()
+    
+test_img = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
+test_lbl =  np.array(list(test_lbl[ 8:])).astype(np.uint8)
+
+print((encoder.inverse_transform(model.predict(test_img)) == test_lbl).sum())
+
