5202bc2641219459d52c6bcef2fa66ff5bac7913
sarahc123 <g00330821@gmit.ie>
2018-11-26 18:03:47 +0000

    accuracy 99.05% and loss .3%

--- digitrec.py
+++ digitrec.py
@@ -3,24 +3,30 @@ import keras as kr
 import numpy as np
 import gzip
 
+with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+    train_img = f.read()
+with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+    train_lbl = f.read()
+
 # Start a neural network, building it by layers.
 model = kr.models.Sequential()
 
 # Add a hidden layer with 1000 neurons and an input layer with 784.
 model.add(kr.layers.Dense(units=600, activation='linear', input_dim=784))
 model.add(kr.layers.Dense(units=500, activation='sigmoid', input_dim=784))
+model.add(kr.layers.Dense(units=400, activation='linear', input_dim=784))
 model.add(kr.layers.Dense(units=300, activation='relu', input_dim=784))
+model.add(kr.layers.Dense(units=150, activation='tanh', input_dim=784))
+
+
 # Add a three neuron output layer.
-model.add(kr.layers.Dense(units=10, activation='softmax'))
+model.add(kr.layers.Dense(units=10, activation='sigmoid'))
 
 # Build the graph.
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
 
-with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
-    train_img = f.read()
-with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
-    train_lbl = f.read()
+
     
 train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8)/255.0
 train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
@@ -34,7 +40,7 @@ encoder = pre.LabelBinarizer()
 encoder.fit(train_lbl)
 outputs = encoder.transform(train_lbl)
 
-model.fit(inputs, outputs, epochs=7, batch_size=98)
+model.fit(inputs, outputs, epochs=10, batch_size=98)
 
 with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
     test_img = f.read()
